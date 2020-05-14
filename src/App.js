106b1d63cb8cb35246e5f04f273aebc8d93c8cdf
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-14 16:17:23 -0700

    working through step 6.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Route } from 'react-router-dom';
 import data from './data';
 
 import { ProductContext } from './contexts/ProductContext';
+// STEP 6 - Providing data with CartContext
+// 6 Let's go ahead and bring our newly created CartContext into our App.js and wrap all of our components inside of our CartContext.Provider. Make sure our ProductContext.Provider is still the root provider.
+import { CartContext } from './contexts/CartContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -27,22 +30,29 @@ function App() {
 			{/* 3 Now that we're providing our products state and addItem function we can simplify our products route a bit. */}
 			{/* 3 After refactoring you'll notice a few errors... Don't worry we'll clean those up shortly! */}
 
-			<ProductContext.Provider value={{ cart, addItem }}>
-				<Navigation />
-			</ProductContext.Provider>
+			{/* 6 Let's go ahead and bring our newly created CartContext into our App.js and wrap all of our components inside of our CartContext.Provider. Make sure our ProductContext.Provider is still the root provider. */}
+			<CartContext.Provider value={[cart]}>
+				<ProductContext.Provider value={{ products, addItem }}>
+					<Navigation />
+				</ProductContext.Provider>
+			</CartContext.Provider>
 
 			{/* Routes */}
-			<ProductContext.Provider value={{ products, addItem }}>
-				<Route exact path="/">
-					<Products />
-				</Route>
-			</ProductContext.Provider>
+			<CartContext.Provider value={[cart]}>
+				<ProductContext.Provider value={{ products, addItem }}>
+					<Route exact path="/">
+						<Products />
+					</Route>
+				</ProductContext.Provider>
+			</CartContext.Provider>
 
-			<ProductContext.Provider value={{ cart, addItem }}>
-				<Route path="/cart">
-					<ShoppingCart />
-				</Route>
-			</ProductContext.Provider>
+			<CartContext.Provider value={[cart]}>
+				<ProductContext.Provider value={{ products, addItem }}>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</ProductContext.Provider>
+			</CartContext.Provider>
 		</div>
 	);
 }
